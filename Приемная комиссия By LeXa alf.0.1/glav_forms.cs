deaea34a653d7524760680cbe13c8ae06fb1ab4e
Harry <eishtein0000@gmail.com>
2023-10-20 15:57:51 +0300

    inti new func

--- Приемная комиссия By LeXa alf.0.1/glav_forms.cs
+++ Приемная комиссия By LeXa alf.0.1/glav_forms.cs
@@ -24,12 +24,40 @@ namespace Приемная_комиссия_By_LeXa
     {
         private bool dataSaved = false;
         private string connectionString = "Data Source=DESKTOP-V7FB61F\\SQLEXPRESS;Initial Catalog=RKRIPT;Integrated Security=True";
-        private Dictionary<string, int> subjectsScores;
+     
+        public Ozenki ozenki = new Ozenki();
 
         public glav_forms()
         {
             InitializeComponent();
-            subjectsScores = new Dictionary<string, int>();
+            FillSpecialtiesComboBox(comboBoxSpecialty1);
+            FillSpecialtiesComboBox(comboBoxSpecialty2);
+            FillSpecialtiesComboBox(comboBoxSpecialty3);
+
+        }
+
+        private void FillSpecialtiesComboBox(ComboBox comboBox)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string query = "SELECT [name_special] FROM special";
+                    SqlCommand command = new SqlCommand(query, connection);
+
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        comboBox.Items.Add(reader["name_special"].ToString());
+                    }
+                    reader.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Произошла ошибка при загрузке специальностей: " + ex.Message);
+            }
         }
 
         private void button7_Click(object sender, EventArgs e)
@@ -45,66 +73,81 @@ namespace Приемная_комиссия_By_LeXa
             // Если результат диалога - OK, сохраняем средний балл в текстовом поле
             if (result == DialogResult.OK)
             {
-                int totalScore = 0;
-                int subjectCount = 0;
-
-                foreach (var score in subjectsScores.Values)
-                {
-                    totalScore += score;
-                    subjectCount++;
-                }
-
-                if (subjectCount > 0)
-                {
-                    float averageScore = (float)totalScore / subjectCount;
-                    txtAverageScore.Text = averageScore.ToString();
-                }
-                else
-                {
-                    MessageBox.Show("Не введены оценки предметов.");
-                }
+                txtAverageScore.Text = ozenki.totalScore.ToString();
+             
             }
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
+            // Получение данных из класса Ozenki
+            float averageScore = ozenki.CalculateAverageScore();
             string fio = txtFIO.Text;
             string selectedSpecialty1 = comboBoxSpecialty1.SelectedItem.ToString();
             string selectedSpecialty2 = comboBoxSpecialty2.SelectedItem.ToString();
             string selectedSpecialty3 = comboBoxSpecialty3.SelectedItem.ToString();
 
-            // Проверка, что были введены оценки
-            sred_ball sredBallForm = new sred_ball();
-            DialogResult result = sredBallForm.ShowDialog();
+            // Путь к рабочему столу пользователя
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
 
-            if (result == DialogResult.OK)
-            {
-                // Получение среднего балла и оценок из формы sred_ball
-                float averageScore = sredBallForm.GetAverageScore();
-                Dictionary<string, int> subjectsScores = sredBallForm.GetSubjectsScores();
+            // Имя файла
+            string fileName = $"{fio}_AverageScore_{averageScore}.docx";
 
-                // Сохранение в базу данных
-                SaveToDatabase(fio, selectedSpecialty1, selectedSpecialty2, selectedSpecialty3, averageScore);
+            // Полный путь к файлу на рабочем столе
+            string filePath = Path.Combine(desktopPath, fileName);
 
-                if (subjectsScores.Count > 0)
-                {
-                    // Создание и открытие Word-документа
-                    string filePath = $"{fio}_AverageScore_{averageScore}.docx";
-                    CreateAndOpenWordDocument(fio, selectedSpecialty1, selectedSpecialty2, selectedSpecialty3, subjectsScores, averageScore, filePath);
-                }
-                else
-                {
-                    MessageBox.Show("Не введены оценки предметов.");
-                }
-            }
-            else
-            {
-                MessageBox.Show("Не введены оценки предметов.");
-            }
+            // Создание текста с оценками за предметы
+            string content = $"ФИО: {fio}\n" +
+                             $"Специальности: {selectedSpecialty1}, {selectedSpecialty2}, {selectedSpecialty3}\n\n" +
+                             $"Оценки за предметы:\n" +
+                             $"Русский язык: {ozenki.russkii}\n" +
+                             $"Литература: {ozenki.literatura}\n" +
+                             $"Родной язык: {ozenki.rodnoiYazik}\n" +
+                             $"Родная литература: {ozenki.rodnoiLiteratura}\n" +
+                             $"Иностранный язык: {ozenki.inostranniiYazik}\n" +
+                             $"История: {ozenki.istoria}\n" +
+                             $"География: {ozenki.geografia}\n" +
+                             $"Алгебра: {ozenki.algebra}\n" +
+                             $"Георафия: {ozenki.geometria}\n" +
+                             $"Информатика: {ozenki.informatika}\n" +
+                             $"Физика: {ozenki.fizika}\n" +
+                             $"Биология: {ozenki.biologia}\n" +
+                             $"Химия: {ozenki.himia}\n" +
+                             $"Изобразительное исскуство: {ozenki.izobrazitelnoeIskusstvo}\n" +
+                             $"Музыка: {ozenki.muzyka}\n" +
+                             $"Технология: {ozenki.tekhnologia}\n" +
+                             $"Физическая культура: {ozenki.fizicheskayaKultura}\n" +
+                             $"Обж: {ozenki.obz}\n" +
+                             // Добавьте остальные предметы
+                             $"\nСредний балл: {averageScore}";
 
-            dataSaved = true;
+            // Сохранение текста с оценками в файл Word на рабочем столе
+            CreateAndOpenWordDocument(content, filePath);
         }
 
+        private void CreateAndOpenWordDocument(string content, string filePath)
+        {
+            try
+            {
+                Word.Application wordApp = new Word.Application();
+                Word.Document doc = wordApp.Documents.Add();
+
+                // Добавление содержимого в документ
+                doc.Content.Text = content;
+
+                // Сохранение документа в файл
+                doc.SaveAs2(filePath);
+                doc.Close();
+                wordApp.Quit();
+
+                // Открытие файла
+                Process.Start(filePath);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Произошла ошибка при создании или открытии документа: " + ex.Message);
+            }
+        }
         private void SaveToDatabase(string fio, string selectedSpecialty1, string selectedSpecialty2, string selectedSpecialty3, float averageScore)
         {
             try
@@ -137,8 +180,9 @@ namespace Приемная_комиссия_By_LeXa
             {
                 MessageBox.Show("Произошла ошибка при сохранении данных в базе данных: " + ex.Message);
             }
-
         }
+
+
 
         private void button3_Click(object sender, EventArgs e)
         {
@@ -146,43 +190,8 @@ namespace Приемная_комиссия_By_LeXa
             special special = new special(); // Передача текущей формы в конструктор glav_forms
             special.Show();
         }
-        private void CreateAndOpenWordDocument(string fio, string selectedSpecialty1, string selectedSpecialty2, string selectedSpecialty3, Dictionary<string, int> subjectsScores, float averageScore, string filePath)
-        {
-            try
-            {
-                Word.Application wordApp = new Word.Application();
-                Word.Document doc = wordApp.Documents.Add();
 
-                // Добавление ФИО и специальностей в документ
-                doc.Content.Text = $"ФИО студента: {fio}\n";
-                doc.Content.Text += $"Специальности: {selectedSpecialty1}, {selectedSpecialty2}, {selectedSpecialty3}\n\n";
-
-                // Добавление таблицы с оценками
-                int rowCount = 1;
-                Word.Table table = doc.Tables.Add(doc.Paragraphs[doc.Paragraphs.Count].Range, subjectsScores.Count, 2);
-
-                foreach (var entry in subjectsScores)
-                {
-                    AddRowToTable(table, rowCount++, entry.Key, entry.Value.ToString());
-                }
-
-                // Добавление среднего балла в документ
-                doc.Content.Text += $"\nСредний балл: {averageScore}\n";
-
-                // Сохранение документа в файл
-                doc.SaveAs2(filePath);
-                doc.Close();
-                wordApp.Quit();
-
-                // Открытие файла
-                Process.Start(filePath);
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Произошла ошибка при создании или открытии документа: " + ex.Message);
-            }
-
-        }
+        
 
         private void AddRowToTable(Word.Table table, int row, string subject, string grade)
         {
--- Приемная комиссия By LeXa alf.0.1/sred_ball.cs
+++ Приемная комиссия By LeXa alf.0.1/sred_ball.cs
@@ -12,152 +12,155 @@ using System.Windows.Forms;
 
 namespace Приемная_комиссия_By_LeXa
 {
-    public partial class sred_ball : Form
-    {
-        public static int russkii { get; set; }
-        public static int literatura { get; set; }
-        public static int rodnoiYazik { get; set; }
-        public static int rodnoiLiteratura { get; set; }
-        public static int inostranniiYazik { get; set; }
-        public static int istoria { get; set; }
-        public static int obchestvo { get; set; }
-        public static int geografia { get; set; }
-        public static int algebra { get; set; }
-        public static int geometria { get; set; }
-        public static int informatika { get; set; }
-        public static int fizika { get; set; }
-        public static int biologia { get; set; }
-        public static int himia { get;  set; }
-        public static int izobrazitelnoeIskusstvo { get;  set; }
-        public static int muzyka { get;  set; }
-        public static int tekhnologia { get;  set; }
-        public static int fizicheskayaKultura { get;  set; }
-        public static int obz { get;  set; }
-        private Dictionary<string, int> subjectsScores;
-        public float GetAverageScore()
-        {
-            return CalculateAverageScore(); 
-        }
-        public Dictionary<string, int> GetSubjectsScores()
-        {
-            Dictionary<string, int> subjectsScores = new Dictionary<string, int>
-        {
-            { "russkii", russkii },
-            { "literatura", literatura },
-            
-        };
-            return subjectsScores;
-        }
+	public partial class sred_ball : Form
+	{
+		//public static int russkii { get; set; }
+		//public static int literatura { get; set; }
+		//public static int rodnoiYazik { get; set; }
+		//public static int rodnoiLiteratura { get; set; }
+		//public static int inostranniiYazik { get; set; }
+		//public static int istoria { get; set; }
+		//public static int obchestvo { get; set; }
+		//public static int geografia { get; set; }
+		//public static int algebra { get; set; }
+		//public static int geometria { get; set; }
+		//public static int informatika { get; set; }
+		//public static int fizika { get; set; }
+		//public static int biologia { get; set; }
+		//public static int himia { get;  set; }
+		//public static int izobrazitelnoeIskusstvo { get;  set; }
+		//public static int muzyka { get;  set; }
+		//public static int tekhnologia { get;  set; }
+		//public static int fizicheskayaKultura { get;  set; }
+		//public static int obz { get;  set; }
+		//private Dictionary<string, int> subjectsScores;
+		//public float GetAverageScore()
+		//{
+		//    return CalculateAverageScore(); 
+		//}
+		//public Dictionary<string, int> GetSubjectsScores()
+		//{
+		//    Dictionary<string, int> subjectsScores = new Dictionary<string, int>
+		//{
+		//    { "russkii", russkii },
+		//    { "literatura", literatura },
 
-        private string connectionString = "Data Source=DESKTOP-V7FB61F\\SQLEXPRESS;Initial Catalog=RKRIPT;Integrated Security=True";
-        public sred_ball()
-        {
-            InitializeComponent();
-            subjectsScores = new Dictionary<string, int>();
+		//};
+		//    return subjectsScores;
+		//}
 
-        }
+		private string connectionString = "Data Source=DESKTOP-V7FB61F\\SQLEXPRESS;Initial Catalog=RKRIPT;Integrated Security=True";
+		public sred_ball()
+		{
+			InitializeComponent();
+			//subjectsScores = new Dictionary<string, int>();
 
+		}
 
-        public static float _averageScore;
-        private void button1_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                // Получение значений из текстовых полей и сохранение их в статические переменные
-                russkii = Convert.ToInt32(txtRusskii.Text);
-                literatura = Convert.ToInt32(txtLiteratura.Text);
-                rodnoiYazik = Convert.ToInt32(txtRodnoiYazik.Text);
-                rodnoiLiteratura = Convert.ToInt32(txtRodnoiLiteratura.Text);
-                inostranniiYazik = Convert.ToInt32(txtInostranniiYazik.Text);
-                istoria = Convert.ToInt32(txtIstoria.Text);
-                obchestvo = Convert.ToInt32(txtObchestvo.Text);
-                geografia = Convert.ToInt32(txtGeografia.Text);
-                algebra = Convert.ToInt32(txtAlgebra.Text);
-                geometria = Convert.ToInt32(txtGeometria.Text);
-                informatika = Convert.ToInt32(txtInformatika.Text);
-                fizika = Convert.ToInt32(txtFizika.Text);
-                biologia = Convert.ToInt32(txtBiologia.Text);
-                himia = Convert.ToInt32(txtHimia.Text);
-                izobrazitelnoeIskusstvo = Convert.ToInt32(txtIzobrazitelnoeIskusstvo.Text);
-                muzyka = Convert.ToInt32(txtMuzyka.Text);
-                tekhnologia = Convert.ToInt32(txtTekhnologia.Text);
-                fizicheskayaKultura = Convert.ToInt32(txtFizicheskayaKultura.Text);
-                obz = Convert.ToInt32(txtOBZ.Text);
-                _averageScore = CalculateAverageScore(); // Рассчет среднего балла и сохранение его
-                DialogResult = DialogResult.OK; // Устанавливаем результат диалога на OK
-                this.Close(); // Закрытие формы CalculateAverageForm после сохранения в базу данных
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Произошла ошибка при сохранении среднего балла в базе данных: " + ex.Message);
-            }
-        }
+		Ozenki ozenki = new Ozenki();
 
-        private float CalculateAverageScore()
-        {
-            // Рассчет среднего балла
-            float totalScore = russkii + literatura + rodnoiYazik + rodnoiLiteratura + inostranniiYazik +
-                              istoria + obchestvo + geografia + algebra + geometria + informatika +
-                              fizika + biologia + himia + izobrazitelnoeIskusstvo + muzyka + tekhnologia +
-                              fizicheskayaKultura + obz;
-            return totalScore / 19.0f;
-        }
-        //public static float GetAverageScore()
-        //{
-        //    return _averageScore; // Метод для получения значения среднего балла из других форм
-        //}
-        private void ComputerScienceTextBox_TextChanged(object sender, EventArgs e)
-        {
+		public static float _averageScore;
+		private void button1_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				// Получение значений из текстовых полей и сохранение их в статические переменные
+				ozenki.russkii = Convert.ToInt32(txtRusskii.Text);
+				ozenki.literatura = Convert.ToInt32(txtLiteratura.Text);
+				ozenki.rodnoiYazik = Convert.ToInt32(txtRodnoiYazik.Text);
+				ozenki.rodnoiLiteratura = Convert.ToInt32(txtRodnoiLiteratura.Text);
+				ozenki.inostranniiYazik = Convert.ToInt32(txtInostranniiYazik.Text);
+				ozenki.istoria = Convert.ToInt32(txtIstoria.Text);
+				ozenki.obchestvo = Convert.ToInt32(txtObchestvo.Text);
+				ozenki.geografia = Convert.ToInt32(txtGeografia.Text);
+				ozenki.algebra = Convert.ToInt32(txtAlgebra.Text);
+				ozenki.geometria = Convert.ToInt32(txtGeometria.Text);
+				ozenki.informatika = Convert.ToInt32(txtInformatika.Text);
+				ozenki.fizika = Convert.ToInt32(txtFizika.Text);
+				ozenki.biologia = Convert.ToInt32(txtBiologia.Text);
+				ozenki.himia = Convert.ToInt32(txtHimia.Text);
+				ozenki.izobrazitelnoeIskusstvo = Convert.ToInt32(txtIzobrazitelnoeIskusstvo.Text);
+				ozenki.muzyka = Convert.ToInt32(txtMuzyka.Text);
+				ozenki.tekhnologia = Convert.ToInt32(txtTekhnologia.Text);
+				ozenki.fizicheskayaKultura = Convert.ToInt32(txtFizicheskayaKultura.Text);
+				ozenki.obz = Convert.ToInt32(txtOBZ.Text);
+				_averageScore = ozenki.CalculateAverageScore(); // Рассчет среднего балла и сохранение его
+				DialogResult = DialogResult.OK; // Устанавливаем результат диалога на OK
+				glav_forms glav_Forms = (glav_forms)Application.OpenForms[Application.OpenForms.Count - 2];
+				glav_Forms.ozenki = ozenki;
+				this.Close(); // Закрытие формы CalculateAverageForm после сохранения в базу данных
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("Произошла ошибка при сохранении среднего балла в базе данных: " + ex.Message);
+			}
+		}
 
-        }
-        public void SaveToDatabase(float averageScore)
-        {
-            try
-            {
-                using (SqlConnection connection = new SqlConnection(connectionString))
-                {
-                    connection.Open();
-                    string query = "INSERT INTO sred_ball (russkii, literat, rodnoi_yazik, rodnoi_literat, inostranniiYazik, " +
-                                   "histori, obchestvo, geograf, algebra, geometria, informatika, fizika, biologia, izo, " +
-                                   "myzika, texcologia, fizra, OBZ, sred_ball) VALUES (@russkii, @literat, @rodnoiYazik, " +
-                                   "@rodnoiLiterat, @inostranniiYazik, @istoria, @obchestvo, @geografia, @algebra, " +
-                                   "@geometria, @informatika, @fizika, @biologia, @izo, @myzika, @texcologia, @fizra, @OBZ, @sredBall)";
-                    SqlCommand command = new SqlCommand(query, connection);
-                    command.Parameters.AddWithValue("@russkii", Convert.ToInt32(txtRusskii.Text));
-                    command.Parameters.AddWithValue("@literat", Convert.ToInt32(txtLiteratura.Text));
-                    command.Parameters.AddWithValue("@rodnoiYazik", Convert.ToInt32(txtRodnoiYazik.Text));
-                    command.Parameters.AddWithValue("@rodnoiLiterat", Convert.ToInt32(txtRodnoiLiteratura.Text));
-                    command.Parameters.AddWithValue("@inostranniiYazik", Convert.ToInt32(txtInostranniiYazik.Text));
-                    command.Parameters.AddWithValue("@istoria", Convert.ToInt32(txtIstoria.Text));
-                    command.Parameters.AddWithValue("@obchestvo", Convert.ToInt32(txtObchestvo.Text));
-                    command.Parameters.AddWithValue("@geografia", Convert.ToInt32(txtGeografia.Text));
-                    command.Parameters.AddWithValue("@algebra", Convert.ToInt32(txtAlgebra.Text));
-                    command.Parameters.AddWithValue("@geometria", Convert.ToInt32(txtGeometria.Text));
-                    command.Parameters.AddWithValue("@informatika", Convert.ToInt32(txtInformatika.Text));
-                    command.Parameters.AddWithValue("@fizika", Convert.ToInt32(txtFizika.Text));
-                    command.Parameters.AddWithValue("@biologia", Convert.ToInt32(txtBiologia.Text));
-                    command.Parameters.AddWithValue("@izo", Convert.ToInt32(txtIzobrazitelnoeIskusstvo.Text));
-                    command.Parameters.AddWithValue("@myzika", Convert.ToInt32(txtMuzyka.Text));
-                    command.Parameters.AddWithValue("@texcologia", Convert.ToInt32(txtTekhnologia.Text));
-                    command.Parameters.AddWithValue("@fizra", Convert.ToInt32(txtFizicheskayaKultura.Text));
-                    command.Parameters.AddWithValue("@OBZ", Convert.ToInt32(txtOBZ.Text));
-                    command.Parameters.AddWithValue("@sredBall", averageScore);
+		//private float CalculateAverageScore()
+		//{
+		//    // Рассчет среднего балла
+		//    float totalScore = russkii + literatura + rodnoiYazik + rodnoiLiteratura + inostranniiYazik +
+		//                      istoria + obchestvo + geografia + algebra + geometria + informatika +
+		//                      fizika + biologia + himia + izobrazitelnoeIskusstvo + muzyka + tekhnologia +
+		//                      fizicheskayaKultura + obz;
+		//    return totalScore / 19.0f;
+		//}
+		//public static float GetAverageScore()
+		//{
+		//    return _averageScore; // Метод для получения значения среднего балла из других форм
+		//}
+		private void ComputerScienceTextBox_TextChanged(object sender, EventArgs e)
+		{
 
-                    int rowsAffected  = command.ExecuteNonQuery();
-                    if (rowsAffected > 0)
-                    {
-                        MessageBox.Show("Средний балл успешно сохранен в базе данных.");
-                    }
-                    else
-                    {
-                        MessageBox.Show("Не удалось сохранить средний балл в базе данных.");
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Произошла ошибка при сохранении среднего балла в базе данных: " + ex.Message);
-            }
-        }
-    }
+		}
+		public void SaveToDatabase(float averageScore)
+		{
+			try
+			{
+				using (SqlConnection connection = new SqlConnection(connectionString))
+				{
+					connection.Open();
+					string query = "INSERT INTO sred_ball (russkii, literat, rodnoi_yazik, rodnoi_literat, inostranniiYazik, " +
+								   "histori, obchestvo, geograf, algebra, geometria, informatika, fizika, biologia, izo, " +
+								   "myzika, texcologia, fizra, OBZ, sred_ball) VALUES (@russkii, @literat, @rodnoiYazik, " +
+								   "@rodnoiLiterat, @inostranniiYazik, @istoria, @obchestvo, @geografia, @algebra, " +
+								   "@geometria, @informatika, @fizika, @biologia, @izo, @myzika, @texcologia, @fizra, @OBZ, @sredBall)";
+					SqlCommand command = new SqlCommand(query, connection);
+					command.Parameters.AddWithValue("@russkii", Convert.ToInt32(txtRusskii.Text));
+					command.Parameters.AddWithValue("@literat", Convert.ToInt32(txtLiteratura.Text));
+					command.Parameters.AddWithValue("@rodnoiYazik", Convert.ToInt32(txtRodnoiYazik.Text));
+					command.Parameters.AddWithValue("@rodnoiLiterat", Convert.ToInt32(txtRodnoiLiteratura.Text));
+					command.Parameters.AddWithValue("@inostranniiYazik", Convert.ToInt32(txtInostranniiYazik.Text));
+					command.Parameters.AddWithValue("@istoria", Convert.ToInt32(txtIstoria.Text));
+					command.Parameters.AddWithValue("@obchestvo", Convert.ToInt32(txtObchestvo.Text));
+					command.Parameters.AddWithValue("@geografia", Convert.ToInt32(txtGeografia.Text));
+					command.Parameters.AddWithValue("@algebra", Convert.ToInt32(txtAlgebra.Text));
+					command.Parameters.AddWithValue("@geometria", Convert.ToInt32(txtGeometria.Text));
+					command.Parameters.AddWithValue("@informatika", Convert.ToInt32(txtInformatika.Text));
+					command.Parameters.AddWithValue("@fizika", Convert.ToInt32(txtFizika.Text));
+					command.Parameters.AddWithValue("@biologia", Convert.ToInt32(txtBiologia.Text));
+					command.Parameters.AddWithValue("@izo", Convert.ToInt32(txtIzobrazitelnoeIskusstvo.Text));
+					command.Parameters.AddWithValue("@myzika", Convert.ToInt32(txtMuzyka.Text));
+					command.Parameters.AddWithValue("@texcologia", Convert.ToInt32(txtTekhnologia.Text));
+					command.Parameters.AddWithValue("@fizra", Convert.ToInt32(txtFizicheskayaKultura.Text));
+					command.Parameters.AddWithValue("@OBZ", Convert.ToInt32(txtOBZ.Text));
+					command.Parameters.AddWithValue("@sredBall", averageScore);
+
+					int rowsAffected = command.ExecuteNonQuery();
+					if (rowsAffected > 0)
+					{
+						MessageBox.Show("Средний балл успешно сохранен в базе данных.");
+					}
+					else
+					{
+						MessageBox.Show("Не удалось сохранить средний балл в базе данных.");
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("Произошла ошибка при сохранении среднего балла в базе данных: " + ex.Message);
+			}
+		}
+	}
 }
