c4f80007c7be2fda72877e91d4d3563a752bbe49
Harry <57110073+MRXTOOR@users.noreply.github.com>
2023-10-29 20:11:37 +0300

    Update special.Designer.cs

--- Приемная комиссия By LeXa alf.0.1/special.Designer.cs
+++ Приемная комиссия By LeXa alf.0.1/special.Designer.cs
@@ -30,28 +30,22 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(special));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-<<<<<<< HEAD
             this.listBox1 = new System.Windows.Forms.ListBox();
-=======
             this.listBoxSpecialties = new System.Windows.Forms.ListBox();
->>>>>>> f663bf3cfccef6b754bc86e8cbbdc1a03c707899
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-<<<<<<< HEAD
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-=======
             this.txtName = new System.Windows.Forms.TextBox();
             this.txtCode = new System.Windows.Forms.TextBox();
             this.txtSeats = new System.Windows.Forms.TextBox();
             this.txtFullName = new System.Windows.Forms.RichTextBox();
->>>>>>> f663bf3cfccef6b754bc86e8cbbdc1a03c707899
             this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
@@ -72,7 +66,6 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Список групп";
             // 
-<<<<<<< HEAD
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
@@ -80,7 +73,6 @@
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(185, 199);
             this.listBox1.TabIndex = 0;
-=======
             // listBoxSpecialties
             // 
             this.listBoxSpecialties.FormattingEnabled = true;
@@ -88,20 +80,17 @@
             this.listBoxSpecialties.Name = "listBoxSpecialties";
             this.listBoxSpecialties.Size = new System.Drawing.Size(219, 199);
             this.listBoxSpecialties.TabIndex = 0;
->>>>>>> f663bf3cfccef6b754bc86e8cbbdc1a03c707899
+
             // 
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.tableLayoutPanel1);
-<<<<<<< HEAD
             this.groupBox2.Location = new System.Drawing.Point(215, 12);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(349, 247);
-=======
             this.groupBox2.Location = new System.Drawing.Point(249, 12);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(315, 247);
->>>>>>> f663bf3cfccef6b754bc86e8cbbdc1a03c707899
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Информация о новой группе";
@@ -115,19 +104,16 @@
             this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
             this.tableLayoutPanel1.Controls.Add(this.label4, 0, 3);
-<<<<<<< HEAD
             this.tableLayoutPanel1.Controls.Add(this.textBox1, 1, 0);
             this.tableLayoutPanel1.Controls.Add(this.textBox2, 1, 1);
             this.tableLayoutPanel1.Controls.Add(this.textBox3, 1, 2);
             this.tableLayoutPanel1.Controls.Add(this.richTextBox1, 1, 3);
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 25);
-=======
             this.tableLayoutPanel1.Controls.Add(this.txtName, 1, 0);
             this.tableLayoutPanel1.Controls.Add(this.txtCode, 1, 1);
             this.tableLayoutPanel1.Controls.Add(this.txtSeats, 1, 2);
             this.tableLayoutPanel1.Controls.Add(this.txtFullName, 1, 3);
             this.tableLayoutPanel1.Location = new System.Drawing.Point(6, 25);
->>>>>>> f663bf3cfccef6b754bc86e8cbbdc1a03c707899
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 4;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
@@ -175,55 +161,45 @@
             // 
             // txtName
             // 
-<<<<<<< HEAD
             this.textBox1.Location = new System.Drawing.Point(164, 3);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(179, 20);
             this.textBox1.TabIndex = 4;
-=======
             this.txtName.Location = new System.Drawing.Point(164, 3);
             this.txtName.Name = "txtName";
             this.txtName.Size = new System.Drawing.Size(132, 20);
             this.txtName.TabIndex = 4;
->>>>>>> f663bf3cfccef6b754bc86e8cbbdc1a03c707899
             // 
             // txtCode
             // 
-<<<<<<< HEAD
             this.textBox2.Location = new System.Drawing.Point(164, 29);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(179, 20);
             this.textBox2.TabIndex = 5;
-=======
             this.txtCode.Location = new System.Drawing.Point(164, 29);
             this.txtCode.Name = "txtCode";
             this.txtCode.Size = new System.Drawing.Size(132, 20);
             this.txtCode.TabIndex = 5;
->>>>>>> f663bf3cfccef6b754bc86e8cbbdc1a03c707899
             // 
             // txtSeats
             // 
-<<<<<<< HEAD
             this.textBox3.Location = new System.Drawing.Point(164, 55);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(179, 20);
             this.textBox3.TabIndex = 6;
-=======
+
             this.txtSeats.Location = new System.Drawing.Point(164, 55);
             this.txtSeats.Name = "txtSeats";
             this.txtSeats.Size = new System.Drawing.Size(132, 20);
             this.txtSeats.TabIndex = 6;
->>>>>>> f663bf3cfccef6b754bc86e8cbbdc1a03c707899
             // 
             // txtFullName
             // 
-<<<<<<< HEAD
             this.richTextBox1.Location = new System.Drawing.Point(164, 81);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.Size = new System.Drawing.Size(179, 96);
             this.richTextBox1.TabIndex = 7;
             this.richTextBox1.Text = "";
-=======
             this.txtFullName.Location = new System.Drawing.Point(164, 81);
             this.txtFullName.Name = "txtFullName";
             this.txtFullName.Size = new System.Drawing.Size(132, 96);
@@ -271,7 +247,6 @@
             this.button1.Text = "Удалить информацию о группе";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
->>>>>>> f663bf3cfccef6b754bc86e8cbbdc1a03c707899
             // 
             // groupBox3
             // 
